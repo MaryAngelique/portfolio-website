0e373d44ec8311d4e15abbbc08588b1d423c310e
Angelique Abacajan <ma_utsey@yahoo.com>
2022-07-20 00:17:14 -0400

    Implemented component import in the app js file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import {
 } from "react-router-dom";
 
 import Navigation from "./components/navigation/navigation";
+import Home from "./components/home/home";
+import AboutMe from "./components/about-me/aboutMe";
+import Skills from "./components/skills/skill";
+import Projects from "./components/projects/projects";
+import Contact from "./components/contact/contact";
 
 function App() {
   return (
