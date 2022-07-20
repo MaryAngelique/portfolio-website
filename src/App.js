66d323b975edac1f674ec95f29fca2c92da66bff
Angelique Abacajan <ma_utsey@yahoo.com>
2022-07-19 21:53:42 -0400

    Implemented navigation into app js -- console errors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import {
+    Routes, 
+    Route 
+} from "react-router-dom";
 
 import Navigation from "./components/navigation/navigation";
 
 function App() {
   return (
-    <Routes>
-        <Route path="/">
-            <Route path="navigation" element={Navigation}/>
-        </Route>
-    </Routes>
+    <div>
+      <div className="portfolio-website">
+        <Routes>
+          <Route index path="/" element={<Navigation />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
