f842631850a05c1996f1828ad26f2bbc370c6f7a
Angelique Abacajan <ma_utsey@yahoo.com>
2022-07-19 20:04:34 -0400

    Implemented website logo into the navigation component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Hello, I'm Angelique</h1>
+      <h2>FullStack Web Developer</h2>
+      <h2>FrontEnd Developer</h2>
+      <h2>BackEnd Developer</h2>
     </div>
   );
 }
