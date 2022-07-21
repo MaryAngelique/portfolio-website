07ddf18b7e6798915b210aac39c8e5682060b178
Angelique Abacajan <ma_utsey@yahoo.com>
2022-07-21 19:19:37 -0400

    Implemented filter data to filter projects for BackEnd, FrontEnd, and All projects

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
+const filterData = [
+    {
+        filterId: 1,
+        label: "All",
+    },
+    {
+        filterId: 2,
+        label: "FrontEnd",
+    },
+    {
+        filterId: 3,
+        label: "BackEnd",
+    },
+];
+
 const Projects = () => {
     return(
         <div>
