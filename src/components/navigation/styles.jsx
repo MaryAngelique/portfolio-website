0baa26e33f2d35c03eafdd8bba3a3e4da3682fa8
Angelique Abacajan <ma_utsey@yahoo.com>
2022-07-23 21:17:54 -0400

    Implemented styling to the navigation container with styled components

---
+++ src/components/navigation/styles.jsx
@@ -0,0 +1,17 @@
+import styled, { css } from "styled-components";
+
+export const NavigationContainer = styled.div`
+    background-color: #292826;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.2rem;
+    position: sticky;
+
+    .navigation-logo-container {
+        cursor: pointer;
+        height: 250px;
+        width: 250px;
+    }
+`;
