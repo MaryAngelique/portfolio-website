8a04f9c5a424ce28b1e36c81089c9343112d6665
Angelique Abacajan <ma_utsey@yahoo.com>
2022-07-26 19:01:15 -0400

    Rendered 2 divs side by side

--- src/components/about-me/styles.jsx
+++ src/components/about-me/styles.jsx
@@ -3,6 +3,7 @@ import styled, { css } from "styled-components";
 export const AboutMeContainer = styled.div`
     position: relative;
     height: 100%;
+    width: 100%;
     justify-content: space-evenly;
     align-content: center;
 `;
@@ -10,41 +11,44 @@ export const AboutMeContainer = styled.div`
 export const PersonalInfo = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 20px;
-    padding: 20px;
-    width: 100%;
+    float: left;
+    padding: 10px;
 
     h3 {
         margin-bottom: 10px;
-        font-size: 2rem;
+        font-size: 1.5rem;
         font-weight: 700;
         color: #f9d342;
 
         span {
-        font-size: 1rem;
-        font-weight: 700;
-        letter-spacing: 0.5px;
-        border-bottom: 2px solid #f9d342;
-        position: relative;
-        display: inline-block;
-        margin-right: 16px;
-        padding-bottom: 5px;
-        color: #ffe67c;
-    }
+            font-size: 1rem;
+            font-weight: 700;
+            letter-spacing: 0.5px;
+            border-bottom: 2px solid #f9d342;
+            position: relative;
+            display: inline-block;
+            margin-right: 16px;
+            padding-bottom: 5px;
+            color: #ffe67c;
+        }
+
+        a {
+            text-decoration: none;
+            color: #f9d342;
+        }
     }
 
     
 `;
 
 export const DeveloperDescription = styled.div`
-    display: flex;
-    gap: 20px;
+    float: right;
     padding: 20px;
-    width: 100%;
+    width: 50%;
     
     h2 {
         color: #f9d342;
-        font-size: 1rem;
+        font-size: 1.5rem;
         padding-bottom: 20px;
         border-bottom: 1px dashed #f9d342;
     }
