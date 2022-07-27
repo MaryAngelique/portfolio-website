ea68b9c87b3b2dcdabf7b2d5b79be964687c2841
Angelique Abacajan <ma_utsey@yahoo.com>
2022-07-27 15:34:21 -0400

    Migrated skills data from skills-data.js to skills component

--- src/components/skills/skill.jsx
+++ src/components/skills/skill.jsx
@@ -1,26 +1,152 @@
 import React from "react";
 import Navigation from "../navigation/navigation";
 
-import { skillsData } from "./skills-data";
-
 import { 
     SkillsContainer, 
     SkillsContentContainer, 
     SkillsContentText,
+    Title,
 } from "./style";
+
+const frontEndData = [
+    // FrontEnd Skills
+    {
+        type: "FrontEnd",
+        data: [
+            {
+                name: "HTML"
+            },
+            {
+                name: "CSS"
+            },
+            {
+                name: "ReactJs"
+            },
+            {
+                name: "Redux"
+            },
+            {
+                name: "Context API"
+            },
+            {
+                name: "Axios"
+            },
+            {
+                name: "Bootstrap"
+            },
+            {
+                name: "Material UI"
+            },
+            {
+                name: "DOM Manipulation"
+            },
+            {
+                name: "JSON"
+            },
+            {
+                name: "Ajax"
+            },
+            {
+                name: "JQuery"
+            },
+            {
+                name: "Responsive Web Design"
+            },
+        ]
+    }
+];
+
+    // BackEnd Skills
+const backEndData = [
+    {
+        type: "BackEnd",
+        data: [
+            {
+                name: "Node.js"
+            },
+            {
+                name: "SQL"
+            },
+            {
+                name: "PostgreSQL"
+            },
+            {
+                name: "Express"
+            },
+            {
+                name: "Knex"
+            },
+            {
+                name: "Python"
+            },
+            {
+                name: "Git CLI"
+            },
+            {
+                name: "Heroku CLI"
+            },
+        ]
+    },
+]
+
+const deploymentData = [
+    // Deployment
+    {
+        type: "Deployment",
+        data: [
+            {
+                name: "Heroku"
+            },
+            {
+                name: "Vercel"
+            },
+            {
+                name: "Netlify"
+            },
+        ]
+    },
+]
+
+const additionalSkillsData = [
+    // Additional Skills
+    {
+        type: "Additional Skills",
+        data: [
+            {
+                name: "Agile Project Management"
+            },
+            {
+                name: "Algorithms"
+            },
+            {
+                name: "Architecture"
+            },
+            {
+                name: "Debugging"
+            },
+            {
+                name: "Deployment"
+            },
+            {
+                name: "Troubleshooting"
+            },
+            {
+                name: "Unit Testing"
+            },
+            {
+                name: "Integration Testing"
+            },
+        ]
+    },
+]
 
 const Skills = () => {
     return(
         <SkillsContainer>
             <Navigation/>
+            <Title>Skills</Title>
             <SkillsContentContainer>
-                {skillsData.map((item, i) => (
-                    <SkillsContentText key={i}>
-                        <h3 className="skills-content-text">
-                            {item.type}
-                        </h3>
-                    </SkillsContentText>
-                ))}
+                
             </SkillsContentContainer>
         </SkillsContainer>
     );
