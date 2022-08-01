0b46ef65e636591273071bc8c84e21db5f907288
Angelique Abacajan <ma_utsey@yahoo.com>
2022-07-31 22:09:52 -0400

    Implemented contact container styled component

---
+++ src/components/contact/styles.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ContactContainer = styled.div`
+    position: relative;
+    height: 100%;
+    width: 100%;
+    justify-content: space-evenly;
+    align-content: center;
+`;
+
