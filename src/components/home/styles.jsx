20928f8f9f85f0fe9bb23e1582c220a25e9a3dff
Angelique Abacajan <ma_utsey@yahoo.com>
2022-07-31 20:57:42 -0400

    Implemented image opacity and scale in home component

--- src/components/home/styles.jsx
+++ src/components/home/styles.jsx
@@ -3,15 +3,17 @@ import styled, { css } from "styled-components";
 export const HomeContainer = styled.div `
     position: relative;
     height: 100%;
-    justify-content: space-evenly;
-    align-content: center;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
 `;
 
 export const HomeTitle = styled.div `
     color: #f9d342;
     float: left;
     justify-content: center;
-    margin: 0px;
     font-size: 2rem;
     display: flex;
     flex-direction: column;
@@ -43,4 +45,10 @@ export const HomeImage = styled.div`
     border: #f9d342 dotted 5px;
     width: 250px;
     height: auto;
+    opacity: 0.1;
+    
+    :hover {
+        transform: scale(1.1);
+        opacity: 1;
+    }
 `;
